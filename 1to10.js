61d1c95acc72829e73517a124819278835581ecd
Maksim Birulia <birula6666@gmail.com>
2023-06-30 19:27:11 +0300

    1to10

---
+++ 1to10.js
@@ -0,0 +1,18 @@
+
+var a = 1;
+let b = 2;
+
+var c = 3;
+let d = 4;
+console.log('a -> ',a)
+console.log('b -> ',b)
+console.log('c -> ',c)
+console.log('d -> ',d)
+
+function counter (x){
+  let i = 1;
+  while (i <= x) {
+    console.log(i)
+    i++
+  };
+}
