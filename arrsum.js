bb5482e95c90d75c07ea10030b8472036777b248
THE-MAZOXIST <birula6666@gmail.com>
2023-07-07 19:48:10 +0300

    added check

--- arrsum.js
+++ arrsum.js
@@ -5,8 +5,13 @@
 
 function arrsum(array){
 var sum = 0;
-for(var i = 0; i < array.length; i++){
-    sum += array[i];
-    }
-console.log(sum);
+if (typeof sum == 'string') {
+  return 'проверь значения';
+}
+else {
+  for(var i = 0; i < array.length; i++){
+      sum += array[i];
+      }
+  console.log(sum);
+  }
 }
