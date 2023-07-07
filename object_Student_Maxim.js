8bd318ffc314245138bc1c34cc3482a62c15b19d
THE-MAZOXIST <birula6666@gmail.com>
2023-07-07 19:35:39 +0300

    add object_student_maxim programm

---
+++ object_Student_Maxim.js
@@ -0,0 +1,18 @@
+/*
+Создайте объект "Student_Maxim" с различными свойствами:
+(имя, возраст, рост, вес и т.д.).
+*/
+
+function fnStudent_Maxim (x){
+  let Student_Maxim = {
+    name: "Maxim",
+    age: 24,
+    weight: 76,
+    height: 186,
+    vision: 1.5,
+    hair_color: 'brown',
+    skin: 'sunburnt',
+    nationality: 'belarus'
+  }
+  return(Student_Maxim)
+}
